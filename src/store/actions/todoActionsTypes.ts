02110a242a03bf1c1999aec757ce36c485272492
dkatorza <dkatorza@gmail.com>
2022-01-04 13:51:58 +0200

    Created todoActionTypes interfaces

--- src/store/actions/todoActionsTypes.ts
+++ src/store/actions/todoActionsTypes.ts
@@ -1,2 +1,25 @@
-import {Todo} from './Todo'
+import { Itodo } from '../types/Itodo';
 
+export const SET_TODOS = 'SET_TODOS';
+export const REMOVE_TODO = 'REMOVE_TODOS';
+export const ADD_TODO = 'ADD_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+
+interface TodoActions {
+  todos: Itodo[];
+}
+
+interface SetTodos extends TodoActions {
+  type: typeof SET_TODOS;
+}
+interface RemoveTodo extends TodoActions {
+  type: typeof REMOVE_TODO;
+}
+interface AddTodo extends TodoActions {
+  type: typeof ADD_TODO;
+}
+interface UpdateTodo extends TodoActions {
+  type: typeof UPDATE_TODO;
+}
+
+export type TodoActionsType = SetTodos | RemoveTodo | AddTodo | UpdateTodo;
